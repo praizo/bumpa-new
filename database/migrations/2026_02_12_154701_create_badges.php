b2f05df8e034156c9fddf4d34bab57467a100d22
praizo <tzpraise@gmail.com>
2026-02-12 16:54:51 +0100

    feat(database): setup loyalty system schema and models

---
+++ database/migrations/2026_02_12_154701_create_badges.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('badges', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->integer('required_achievements')->unsigned();
+            $table->integer('cashback_amount')->default(300);
+            $table->json('metadata')->nullable(); // Future-proofing
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('badges');
+    }
+};
